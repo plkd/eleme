79cc180667fbb2cc779783809c02c5c06144badb
plkd <209603537@qq.com>
2017-12-28 00:52:39 +0800

    foot guide component

--- src/page/profile/profile.vue
+++ src/page/profile/profile.vue
@@ -28,11 +28,117 @@
                     </span>
                 </router-link>
             </section>
+            <section class="info-data">
+                <ul class="clear">
+                    <router-link to="/balance" tag="li" class="info-data-link">
+                        <span class="info-data-top">
+                            <b>{{parseInt(balance).toFixed(2)}}</b>元</span>
+                        <span class="info-data-bottom">我的余额</span>
+                    </router-link>
+                    <router-link to="/benefit" tag="li" class="info-data-link">
+                        <span class="info-data-top">
+                            <b>{{count}}</b>个</span>
+                        <span class="info-data-bottom">我的优惠</span>
+                    </router-link>
+                    <router-link to="/points" tag="li" class="info-data-link">
+                        <span class="info-data-top">
+                            <b>{{pointNumber}}</b>分</span>
+                        <span class="info-data-bottom">我的积分</span>
+                    </router-link>
+                </ul>
+            </section>
+            <section class="profile-1reTe">
+                <!-- 我的订单 -->
+                <router-link to="/order" class="myorder">
+                    <aside>
+                        <svg fill="#4aa5f0">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#order"></use>
+                        </svg>
+                    </aside>
+                    <div class="myorder-div">
+                        <span>我的订单</span>
+                        <span class="myorder-divsvg">
+                            <svg fill="#bbb">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+                            </svg>
+                        </span>
+                    </div>
+                </router-link>
+                <!-- 积分商城 -->
+                <a href='https://home.m.duiba.com.cn/#/chome/index' class="myorder">
+                    <aside>
+                        <svg fill="#fc7b53">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#point"></use>
+                        </svg>
+                    </aside>
+                    <div class="myorder-div">
+                        <span>积分商城</span>
+                        <span class="myorder-divsvg">
+                            <svg fill="#bbb">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+                            </svg>
+                        </span>
+                    </div>
+                </a>
+                <!-- 饿了么会员卡 -->
+                <router-link to='/vipcard' class="myorder">
+                    <aside>
+                        <svg fill="#ffc636">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#vip"></use>
+                        </svg>
+                    </aside>
+                    <div class="myorder-div">
+                        <span>饿了么会员卡</span>
+                        <span class="myorder-divsvg">
+                            <svg fill="#bbb">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+                            </svg>
+                        </span>
+                    </div>
+                </router-link>
+            </section>
+            <section class="profile-1reTe">
+                <!-- 服务中心 -->
+                <router-link to='/service' class="myorder">
+                    <aside>
+                        <svg fill="#4aa5f0">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#service"></use>
+                        </svg>
+                    </aside>
+                    <div class="myorder-div">
+                        <span>服务中心</span>
+                        <span class="myorder-divsvg">
+                            <svg fill="#bbb">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+                            </svg>
+                        </span>
+                    </div>
+                </router-link>
+                <!-- 下载饿了么APP -->
+                <router-link to='/download' class="myorder">
+                    <aside>
+                        <svg fill="#3cabff">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#download"></use>
+                        </svg>
+                    </aside>
+                    <div class="myorder-div" style="border-bottom:0;">
+                        <span>下载饿了么APP</span>
+                        <span class="myorder-divsvg">
+                            <svg fill="#bbb">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+                            </svg>
+                        </span>
+                    </div>
+                </router-link>
+            </section>
         </section>
+        <foot-guide></foot-guide>
+
     </div>
 </template>
 <script>
 import headTop from "src/components/header/head";
+import footGuide from "src/components/footer/footGuide";
 import { mapState, mapMutations } from "vuex";
 import { imgBaseUrl } from "src/config/env";
 
@@ -51,7 +157,8 @@ export default {
     };
   },
   components: {
-    headTop
+    headTop,
+    footGuide
   },
   mounted() {
     this.initData();
@@ -145,7 +252,103 @@ export default {
     }
   }
 }
-
+.info-data {
+  width: 100%;
+  background: $fc;
+  box-sizing: border-box;
+  ul {
+    .info-data-link {
+      float: left;
+      width: 33.33%;
+      display: inline-block;
+      border-right: 1px solid #f1f1f1;
+      span {
+        display: block;
+        width: 100%;
+        text-align: center;
+      }
+      .info-data-top {
+        @include sc(0.55rem,#333);
+        padding: 0.853333rem 0 0.453333rem;
+        b {
+          display: inline-block;
+          @include sc(1.2rem,#f90);
+          font-weight: 700;
+          line-height: 1rem;
+          font-family: Helvetica Neue, Tahoma;
+        }
+      }
+      .info-data-bottom {
+        @include sc(0.57333rem,#666);
+        font-weight: 400;
+        padding-bottom: 0.453333rem;
+      }
+    }
+    .info-data-link:nth-of-type(2) {
+      .info-data-top {
+        b {
+          color: #ff5f3e;
+        }
+      }
+    }
+    .info-data-link:nth-of-type(3) {
+      border: 0;
+      .info-data-top {
+        b {
+          color: #6ac20b;
+        }
+      }
+    }
+  }
+}
+.profile-1reTe {
+  margin-top: 0.4rem;
+  background: $fc;
+  .myorder {
+    padding-left: 1.6rem;
+    display: flex;
+    align-items: center;
+    aside {
+      @include wh(0.7rem,0.7rem);
+      margin-left: -0.866667rem;
+      margin-right: 0.266667rem;
+      display: flex;
+      align-items: center;
+      svg {
+        @include wh(100%,100%);
+      }
+    }
+    .myorder-div {
+      width: 100%;
+      border-bottom: 1px solid #f1f1f1;
+      padding: 0.433333rem 0.266667rem 0.433333rem 0;
+      @include sc(0.7rem,#333);
+      display: flex;
+      justify-content: space-between;
+      span {
+        display: block;
+      }
+      .myorder-divsvg {
+        @include wh(0.46667rem,0.466667rem);
+        svg {
+          @include wh(100%,100%);
+        }
+      }
+    }
+  }
+  .myorder:nth-of-type(3) .myorder-div {
+    border: 0;
+  }
+}
+.router-slid-enter-active,
+.router-slid-leave-active {
+  transition: all 0.4s;
+}
+.router-slid-enter,
+.router-slid-leave-active {
+  transform: translate3d(2rem, 0, 0);
+  opacity: 0;
+}
 </style>
 
 
